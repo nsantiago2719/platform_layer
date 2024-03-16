beb49dd99ce5dd4494c90ef6da7333f6c90bf4ee
Norman Santiago <nsantiago2719@gmail.com>
2024-03-16 22:49:26 +0800

    move out initial djago files outside directory

--- platform_layer/asgi.py
+++ platform_layer/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'platform_layer.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "platform_layer.settings")
 
 application = get_asgi_application()
